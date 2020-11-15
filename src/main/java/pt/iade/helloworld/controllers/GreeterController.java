8d80cf2d0134b08ed087870a5bd45ab4af855cf8
João <joaopedroandre2000@gmail.com>
2020-11-15 18:43:18 +0000

    part 2

--- src/main/java/pt/iade/helloworld/controllers/GreeterController.java
+++ src/main/java/pt/iade/helloworld/controllers/GreeterController.java
@@ -7,15 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
-@RequestMapping(path="/api/greeter")
-public class GreeterController {
- private Logger logger = LoggerFactory.getLogger(GreeterController.class);
- @GetMapping(path = "{name}",
- produces= MediaType.APPLICATION_JSON_VALUE)
- public String getGreeting(@PathVariable("name") String name)  {
- logger.info("Saying Hello to" +name);
- return "Hello " + name;
- }
-}
 
+@RestController
+@RequestMapping(path = "/api/greeter")
+public class GreeterController {
+    private Logger logger = LoggerFactory.getLogger(GreeterController.class);
+
+    @GetMapping(path = "{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getGreeting(@PathVariable("name") String name) {
+        logger.info("Saying Hello to" + name);
+        return "Hello " + name;
+    }
+
+    @GetMapping(path = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getGreeting() {
+        logger.info("Saying Hello to world");
+        return "Hello to world";
+    }
+
+}
